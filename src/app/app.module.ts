8dad7a4422cb3591b9690dff8b07cb0bed85f42c
Miguel319 <miguelp90.319@gmail.com>
2019-08-24 21:31:36 -0400

    Listado PC + formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { PcListaComponent } from './componentes/pc-lista/pc-lista.component';
-import { PcComponent } from './componentes/pc/pc.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { PcListaComponent } from "./componentes/pc-lista/pc-lista.component";
+import { PcComponent } from "./componentes/pc/pc.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PcListaComponent,
-    PcComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PcListaComponent, PcComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
