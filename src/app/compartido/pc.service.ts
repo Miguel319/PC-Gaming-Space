8f5d0aa706b21637c5a5193862f0cea0a3319565
Miguel319 <miguelp90.319@gmail.com>
2019-08-24 22:46:08 -0400

    Actualización de PCs + Sweetalert2

--- src/app/compartido/pc.service.ts
+++ src/app/compartido/pc.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { PC } from '../modelos/pc.model';
-import { map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { PC } from "../modelos/pc.model";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
@@ -12,12 +12,27 @@ export class PcService {
   constructor(private http: HttpClient) {}
 
   agregarPC(pc: PC) {
-    return this.http.post(`${this.url}/pcs.json`, pc)
-      .pipe(
-        map((res: any)=> {
-          pc.id = res.name;
-          return pc;
-        })
-      )
+    const body = JSON.stringify(pc);
+
+    const headers = new HttpHeaders({
+      "Content-Type": "application/json"
+    });
+
+    return this.http.post(`${this.url}/pcs.json`, body, { headers }).pipe(
+      map((res: any) => {
+        pc.id = res.name;
+        return pc;
+      })
+    );
+  }
+
+  actualizarPC(pc: PC) {
+    const pcTemp: PC = {
+      ...pc
+    };
+
+    delete pcTemp.id;
+
+    return this.http.put(`${this.url}/pcs/${pc.id}.json`, pcTemp);
   }
 }
--- src/app/componentes/pc/pc.component.ts
+++ src/app/componentes/pc/pc.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { PC } from "src/app/modelos/pc.model";
 import { NgForm } from "@angular/forms";
 import { PcService } from "src/app/compartido/pc.service";
+import Swal from "sweetalert2";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-pc",
@@ -23,10 +25,28 @@ export class PcComponent implements OnInit {
       return;
     }
 
-    this.pcService.agregarPC(this.pc).subscribe((res: PC) => {
-      console.log(res);
-      this.pc = res;
-    }),
-      err => console.log(err);
+    Swal.fire({
+      title: "Espere",
+      text: "Guardando..",
+      type: "info",
+      allowOutsideClick: false
+    });
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+
+    if (this.pc.id) {
+      peticion = this.pcService.actualizarPC(this.pc);
+    } else {
+      peticion = this.pcService.agregarPC(this.pc);
+    }
+
+    peticion.subscribe(res => {
+      Swal.fire({
+        title: this.pc.alias,
+        text: "¡PC Gaming actualizada satisfactoriamente!",
+        type: "success"
+      });
+    });
   }
 }
